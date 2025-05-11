3d0b01d8688bbda976b05ac5dfb3fb54212bf3cc
Kalahan Santhiago <ksodesign@gmail.com>
2025-05-11 04:09:38 -0300

    Gravando os dados em banco antes de iniciar a aplicação

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import app from './src/app.js';
+import { loadWinners } from './src/csv/parseWinnersAwards.js';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Runing in http://localhost:${PORT}/producers/intervals`);
-});
+loadWinners()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Runing in http://localhost:${PORT}/producers/intervals`);
+    });
+  })
+  .catch((err) => {
+    // TODO:: Retornar erro tratado em json
+    console.error('Error:', err);
+  });
