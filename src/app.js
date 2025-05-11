4b4dad0d20273c1e554446b9b07cff42d4b81e41
Kalahan Santhiago <ksodesign@gmail.com>
2025-05-11 03:15:22 -0300

    Tratando erro para metodos não permitidos

--- src/app.js
+++ src/app.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(express.json());
 app.use('/producers/intervals', intervalsRouter);
 
+app.all('/producers/intervals', (req, res) => {
+    res.status(405).json({ error: 'Method Not Allowed' });
+});
+
 export default app;
