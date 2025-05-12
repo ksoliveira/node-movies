af1cc62add619501bfa7452555d941ffcf5bace2
Kalahan Santhiago <ksodesign@gmail.com>
2025-05-11 21:55:30 -0300

    Testes de integração para cenários de http status 200 e 204

---
+++ tests/routes/intervals.test.js
@@ -0,0 +1,30 @@
+import request from 'supertest';
+import app from '../../src/app.js';
+import db from '../../src/database/database.js';
+
+describe('GET /producers/intervals', () => {
+  beforeEach(() => {
+    db.exec('DELETE FROM movies');
+  });
+
+  it('deve retornar 204 se não houver intervalos', async () => {
+    const response = await request(app).get('/producers/intervals');
+    expect(response.status).toBe(204);
+  });
+
+  it('deve retornar 200 e os intervalos de produtores', async () => {
+    db.exec(`
+      INSERT INTO movies (year, title, studios, producers, winner)
+      VALUES
+        (1980, 'Movie 1', 'Studio 1', 'Producer 1', 'yes'),
+        (1981, 'Movie 2', 'Studio 2', 'Producer 1', 'yes'),
+        (1980, 'Movie 3', 'Studio 3', 'Producer 2', 'yes'),
+        (1920, 'Movie 4', 'Studio 4', 'Producer 2', 'yes');
+    `);
+
+    const response = await request(app).get('/producers/intervals');
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('min');
+    expect(response.body).toHaveProperty('max');
+  });
+});
