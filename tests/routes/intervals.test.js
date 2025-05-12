472c0453259f1ec47befb2b507f7aecd519d9f38
Kalahan Santhiago <ksodesign@gmail.com>
2025-05-11 22:57:00 -0300

    Cenário de teste com dois produtores empatados em min e max

--- tests/routes/intervals.test.js
+++ tests/routes/intervals.test.js
@@ -8,11 +8,6 @@ describe('GET /producers/intervals', () => {
     db.exec('DELETE FROM movies');
   });
 
-  it('deve retornar 204 se não houver intervalos', async () => {
-    const response = await request(app).get('/producers/intervals');
-    expect(response.status).toBe(204);
-  });
-
   it('deve retornar 200 e os intervalos de produtores', async () => {
     db.exec(`
       INSERT INTO movies (year, title, studios, producers, winner)
@@ -27,6 +22,34 @@ describe('GET /producers/intervals', () => {
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('min');
     expect(response.body).toHaveProperty('max');
+  });
+
+  it('deve retornar 200 e ter dois produtores empatados no MIN e no MAX', async () => {
+    db.exec(`
+      INSERT INTO movies (year, title, studios, producers, winner)
+      VALUES
+        (1980, 'Movie 1', 'Studio 1', 'Producer 1', 'yes'),
+        (1981, 'Movie 2', 'Studio 2', 'Producer 1', 'yes'),
+        (1980, 'Movie 3', 'Studio 3', 'Producer 2', 'yes'),
+        (1920, 'Movie 4', 'Studio 4', 'Producer 2', 'yes'),
+        (1980, 'Movie 5', 'Studio 5', 'Producer 3', 'yes'),
+        (1981, 'Movie 6', 'Studio 6', 'Producer 3', 'yes'),
+        (1940, 'Movie 7', 'Studio 7', 'Producer 4', 'yes'),
+        (2000, 'Movie 8', 'Studio 8', 'Producer 4', 'yes'),
+        (2000, 'Movie 9', 'Studio 9', 'Producer 5', 'yes');
+    `);
+
+    const response = await request(app).get('/producers/intervals');
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('min');
+    expect(response.body).toHaveProperty('max');
+    expect(response.body.min.length).toEqual(2);
+    expect(response.body.max.length).toEqual(2);
+  });
+
+  it('deve retornar 204 se não houver intervalos', async () => {
+    const response = await request(app).get('/producers/intervals');
+    expect(response.status).toBe(204);
   });
 
   it('deve retornar 404 para uma rota inexistente', async () => {
@@ -51,28 +74,5 @@ describe('GET /producers/intervals', () => {
     expect(response.body).toEqual({ error: 'Internal server error.' });
   
     db.prepare.mockRestore();
-  });
-
-  it('deve retornar 200 e ter dois produtores empatados no MIN e no MAX', async () => {
-    db.exec(`
-      INSERT INTO movies (year, title, studios, producers, winner)
-      VALUES
-        (1980, 'Movie 1', 'Studio 1', 'Producer 1', 'yes'),
-        (1981, 'Movie 2', 'Studio 2', 'Producer 1', 'yes'),
-        (1980, 'Movie 3', 'Studio 3', 'Producer 2', 'yes'),
-        (1920, 'Movie 4', 'Studio 4', 'Producer 2', 'yes'),
-        (1980, 'Movie 5', 'Studio 5', 'Producer 3', 'yes'),
-        (1981, 'Movie 6', 'Studio 6', 'Producer 3', 'yes'),
-        (1940, 'Movie 7', 'Studio 7', 'Producer 4', 'yes'),
-        (2000, 'Movie 8', 'Studio 8', 'Producer 4', 'yes'),
-        (2000, 'Movie 9', 'Studio 9', 'Producer 5', 'yes');
-    `);
-
-    const response = await request(app).get('/producers/intervals');
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('min');
-    expect(response.body).toHaveProperty('max');
-    expect(response.body.min.length).toEqual(2);
-    expect(response.body.max.length).toEqual(2);
   });
 });
